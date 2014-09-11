11a91897b5020139b955f776a6183f695e0d9426
Oleg Hahm <oleg@hobbykeller.org>
2014-09-11 18:19:13 +0200

    licenses: add missing and fix broken license headers

--- sys/net/include/udp.h
+++ sys/net/include/udp.h
@@ -1,11 +1,9 @@
-/**
- * udp.h - Wraps all API types, constants and functions of the tcp implementation.
+/*
+ * Copyright (C) 2013, 2014 INRIA.
  *
- * Copyright (C) 2013  INRIA.
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 /**
