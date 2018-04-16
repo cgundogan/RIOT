ce374ca4d6b584b63c0bb523f223bb61647ca21b
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2018-04-16 11:17:13 +0200

    ndn_*: print rank and macid correctly

--- examples/ndn_vanilla/main.c
+++ examples/ndn_vanilla/main.c
@@ -665,7 +665,6 @@ int main(void)
     gnrc_netapi_get(netif->pid, NETOPT_ADDRESS_LONG, 0, my_hwaddr, sizeof(my_hwaddr));
 #endif
     gnrc_netif_addr_to_str(my_hwaddr, sizeof(my_hwaddr), my_hwaddr_str);
-    printf("My ID is: %s\n", my_hwaddr_str);
 
     for (int i = 0; i < MACMAPSZ; i++) {
         if (!strcmp(my_hwaddr_str, macmap[i])) {
@@ -675,6 +674,8 @@ int main(void)
     }
 
     snprintf(my_macid_str, sizeof(my_macid_str), "%03d", my_macid);
+
+    printf("hwaddr: %s, macid: %s\n", my_hwaddr_str, my_macid_str);
 
 #ifdef MODULE_HOPP
     hopp_netif = netif;
