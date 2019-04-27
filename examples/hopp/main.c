2fc970f633331bbd24e8e9268354900c8985fa16
Cenk Gündoğan <mail+dev@gundogan.net>
2019-04-27 22:45:43 +0200

    some major changes to include a static routing topology

--- examples/hopp/main.c
+++ examples/hopp/main.c
@@ -31,6 +31,12 @@ uint8_t hwaddr[GNRC_NETIF_L2ADDR_MAXLEN];
 char hwaddr_str[GNRC_NETIF_L2ADDR_MAXLEN * 3];
 bool i_am_root = false;
 
+#ifndef CONSUMER_STACKSZ
+#define CONSUMER_STACKSZ (THREAD_STACKSIZE_DEFAULT + THREAD_EXTRA_STACKSIZE_PRINTF)
+#endif
+
+char consumer_stack[CONSUMER_STACKSZ];
+
 /* 10kB buffer for the heap should be enough for everyone */
 #ifndef TLSF_BUFFER
 #define TLSF_BUFFER     (10240)
@@ -41,9 +47,10 @@ static uint32_t _tlsf_heap[TLSF_BUFFER / sizeof(uint32_t)];
 #ifndef ROOTADDR
 #define ROOTADDR "15:11:6B:10:65:FD:AC:52"
 #endif
-#ifndef ROOTPREFIX
-#define ROOTPREFIX "/HK"
+#ifndef ROOTPFX
+#define ROOTPFX "HK"
 #endif
+#define ROOTPREFIX ("/" ROOTPFX)
 #ifndef CONSUMER_THREAD_PRIORITY
 #define CONSUMER_THREAD_PRIORITY (THREAD_PRIORITY_MAIN - 1)
 #endif
@@ -55,11 +62,11 @@ static uint32_t _tlsf_heap[TLSF_BUFFER / sizeof(uint32_t)];
 #endif
 
 #ifndef DELAY_REQUEST
-#define DELAY_REQUEST           (5 * 1000000) // us = 30sec
+#define DELAY_REQUEST           (120 * 1000000) // us = 30sec
 #endif
 
 #ifndef DELAY_JITTER
-#define DELAY_JITTER            (2 * 1000000) // us = 15sec
+#define DELAY_JITTER            (60 * 1000000) // us = 15sec
 #endif
 
 #define DELAY_MAX               (DELAY_REQUEST + DELAY_JITTER)
@@ -69,10 +76,19 @@ static uint32_t _tlsf_heap[TLSF_BUFFER / sizeof(uint32_t)];
 #define REQ_DELAY               (random_uint32_range(DELAY_MIN, DELAY_MAX))
 #endif
 
+#ifndef REQ_NUMS
+#define REQ_NUMS (1)
+#endif
+
 static unsigned char int_buf[CCNL_MAX_PACKET_SIZE];
 static unsigned char data_buf[CCNL_MAX_PACKET_SIZE];
 
 static const char *rootaddr = ROOTADDR;
+static const char *rootprefix = ROOTPREFIX;
+
+uint64_t the_time = 0;
+uint32_t num_ints = 0;
+uint32_t num_datas = 0;
 
 #define QOS_MAX_TC_ENTRIES (3)
 
@@ -83,7 +99,57 @@ static const qos_traffic_class_t tcs[QOS_MAX_TC_ENTRIES] =
     { "/HK/sensors", false, true },
 };
 
-int pit_strategy(struct ccnl_relay_s *relay, struct ccnl_interest_s *i)
+static void show_fibs(struct ccnl_relay_s *relay) __attribute((used));
+static void show_fibs(struct ccnl_relay_s *relay)
+{
+    char s[CCNL_MAX_PREFIX_SIZE];
+    for (struct ccnl_forward_s *fwd = relay->fib; fwd; fwd = fwd->next) {
+        printf("fwd;%s;%s\n", ccnl_prefix_to_str(fwd->prefix,s,CCNL_MAX_PREFIX_SIZE), ccnl_addr2ascii(&fwd->face->peer));
+    }
+}
+
+static struct ccnl_face_s *_intern_face_get(char *addr_str)
+{
+    uint8_t relay_addr[GNRC_NETIF_L2ADDR_MAXLEN];
+    memset(relay_addr, UINT8_MAX, GNRC_NETIF_L2ADDR_MAXLEN);
+    size_t addr_len = gnrc_netif_addr_from_str(addr_str, relay_addr);
+
+    if (addr_len == 0) {
+        printf("Error: %s is not a valid link layer address\n", addr_str);
+        return NULL;
+    }
+
+    sockunion sun;
+    sun.sa.sa_family = AF_PACKET;
+    memcpy(&(sun.linklayer.sll_addr), relay_addr, addr_len);
+    sun.linklayer.sll_halen = addr_len;
+    sun.linklayer.sll_protocol = htons(ETHERTYPE_NDN);
+
+    return ccnl_get_face_or_create(&ccnl_relay, 0, &sun.sa, sizeof(sun.linklayer));
+}
+
+static void add_fib(char *pfx, char *addr)
+{
+    char *prefix_str[64];
+    memset(prefix_str, 0, sizeof(prefix_str));
+    memcpy(prefix_str, pfx, strlen(pfx));
+
+    int suite = CCNL_SUITE_NDNTLV;
+    struct ccnl_prefix_s *prefix = ccnl_URItoPrefix((char *)prefix_str, suite, NULL);
+    struct ccnl_face_s *fibface = _intern_face_get(addr);
+    fibface->flags |= CCNL_FACE_FLAGS_STATIC;
+    ccnl_fib_add_entry(&ccnl_relay, prefix, fibface);
+}
+
+static void setup_forwarding(char *my_addr) __attribute__((used));
+static void setup_forwarding(char *my_addr)
+{
+    #include "fibs.in"
+
+    return;
+}
+
+static int pit_strategy(struct ccnl_relay_s *relay, struct ccnl_interest_s *i)
 {
     qos_traffic_class_t *tc = i->tc;
 
@@ -149,16 +215,18 @@ int pit_strategy(struct ccnl_relay_s *relay, struct ccnl_interest_s *i)
     return 0;
 }
 
+/*
 static int _root(int argc, char **argv)
 {
     (void) argc;
     (void) argv;
 
-    hopp_root_start(rootaddr, strlen(rootaddr));
+    hopp_root_start(rootprefix, strlen(rootprefix));
     i_am_root = true;
     return 0;
 }
-
+*/
+/*
 static int _publish(int argc, char **argv)
 {
     (void)argc;
@@ -170,17 +238,21 @@ static int _publish(int argc, char **argv)
 
     char name[30];
     int name_len = sprintf(name, "%s/sensors/%s", ROOTPREFIX, hwaddr_str);
-    xtimer_usleep(random_uint32_range(0, 40000000));
+    xtimer_usleep(random_uint32_range(0, 60000000));
     hopp_publish_content(name, name_len, NULL, 0);
     return 0;
 }
+*/
 
+/*
 static const shell_command_t shell_commands[] = {
     { "hr", "start HoPP root", _root },
     { "hp", "publish data", _publish },
     { NULL, NULL, NULL }
 };
+*/
 
+static void cb_published(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, struct ccnl_face_s *from) __attribute((used));
 static void cb_published(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, struct ccnl_face_s *from)
 {
     from->flags |= CCNL_FACE_FLAGS_STATIC;
@@ -196,6 +268,7 @@ static uint32_t _count_fib_entries(void) {
     return num_fib_entries;
 }
 
+static void *consumer_event_loop(void *arg) __attribute__((used));
 static void *consumer_event_loop(void *arg)
 {
     (void)arg;
@@ -208,24 +281,28 @@ static void *consumer_event_loop(void *arg)
 
     printf("consumer_setup;%d\n",nodes_num);
 
-    for (unsigned i = 0; i < 100; i++) {
+    for (unsigned i = 0; i < REQ_NUMS; i++) {
         for (fwd = ccnl_relay.fib; fwd; fwd = fwd->next) {
-            memset(int_buf, 0, 64);
+            memset(int_buf, 0, CCNL_MAX_PACKET_SIZE);
             delay = (uint32_t)((float)REQ_DELAY/(float)nodes_num);
             xtimer_usleep(delay);
             ccnl_prefix_to_str(fwd->prefix,s,CCNL_MAX_PREFIX_SIZE);
             snprintf(req_uri, 64, "%s/%04d", s, i);
-            printf("req;%s\n",req_uri);
+            num_ints++;
+            printf("req;%s;%lu;%lu\n", req_uri, (unsigned long) num_ints, (unsigned long) num_datas);
             prefix = ccnl_URItoPrefix(req_uri, CCNL_SUITE_NDNTLV, NULL);
-            ccnl_send_interest(fwd->prefix, int_buf, HOPP_INTEREST_BUFSIZE, NULL, NULL);
+            ccnl_send_interest(prefix, int_buf, CCNL_MAX_PACKET_SIZE, NULL, NULL);
             ccnl_prefix_free(prefix);
         }
     }
-    printf("consumer_done\n");
+
+    xtimer_sleep(10);
+    printf("consumer_done,%lu,%lu\n", (unsigned long) num_ints, (unsigned long) num_datas);
 
     return 0;
 }
-int produce_cont_and_cache(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, int id)
+
+static int produce_cont_and_cache(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, int id)
 {
     (void)pkt;
     char name[64];
@@ -235,7 +312,7 @@ int produce_cont_and_cache(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, i
     size_t len = sprintf(buffer, "%s", "24.5");
     buffer[len]='\0';
 
-    int name_len = sprintf(name, "/%s/sensors/%s/%04d", ROOTPREFIX, hwaddr_str, id);
+    int name_len = sprintf(name, "/%s/sensors/%s/%04d", ROOTPFX, hwaddr_str, id);
     name[name_len]='\0';
 
     struct ccnl_prefix_s *prefix = ccnl_URItoPrefix(name, CCNL_SUITE_NDNTLV, NULL);
@@ -251,12 +328,14 @@ int produce_cont_and_cache(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, i
 
     if (ccnl_ndntlv_dehead(&data, &reslen, &typ, &len) || typ != NDN_TLV_Data) {
         puts("ERROR in producer_func");
-        return -1;
+        return 0;
     }
 
     struct ccnl_content_s *c = 0;
     struct ccnl_pkt_s *pk = ccnl_ndntlv_bytes2pkt(typ, olddata, &data, &reslen);
     c = ccnl_content_new(&pk);
+    c->flags |= CCNL_CONTENT_FLAGS_STATIC;
+    puts("ADD2CACHE");
     ccnl_content_add2cache(relay, c);
     return 0;
 }
@@ -264,9 +343,17 @@ int produce_cont_and_cache(struct ccnl_relay_s *relay, struct ccnl_pkt_s *pkt, i
 int producer_func(struct ccnl_relay_s *relay, struct ccnl_face_s *from, struct ccnl_pkt_s *pkt){
     (void) relay;
     (void) from;
+//    puts("PRODUCE");
+
     if(pkt->pfx->compcnt == 4) { /* /PREFIX/sensors/ID/<value> */
-        /* match PREFIX and ID and "gasval" */
-        if (!memcmp(pkt->pfx->comp[0], ROOTPREFIX, pkt->pfx->complen[0]) &&
+/*
+        printf("%.*s / %.*s / %.*s / %.*s\n",
+               pkt->pfx->complen[0], pkt->pfx->comp[0],
+               pkt->pfx->complen[1], pkt->pfx->comp[1],
+               pkt->pfx->complen[2], pkt->pfx->comp[2],
+               pkt->pfx->complen[3], pkt->pfx->comp[3]);
+*/
+        if (!memcmp(pkt->pfx->comp[0], ROOTPFX, pkt->pfx->complen[0]) &&
             !memcmp(pkt->pfx->comp[1], "sensors", pkt->pfx->complen[1]) &&
             !memcmp(pkt->pfx->comp[2], hwaddr_str, pkt->pfx->complen[2])) {
             return produce_cont_and_cache(relay, pkt, atoi((const char *)pkt->pfx->comp[3]));
@@ -284,23 +371,26 @@ int main(void)
 
     ccnl_start();
 
-    if (((hopp_netif = gnrc_netif_iter(NULL)) == NULL) ||
-        (ccnl_open_netif(hopp_netif->pid, GNRC_NETTYPE_CCN) < 0)) {
+    gnrc_netif_t *ccn_netif;
+
+    if (((ccn_netif = gnrc_netif_iter(NULL)) == NULL) ||
+        (ccnl_open_netif(ccn_netif->pid, GNRC_NETTYPE_CCN) < 0)) {
         return -1;
     }
 
     uint16_t chan = 11;
-    gnrc_netapi_set(hopp_netif->pid, NETOPT_CHANNEL, 0, &chan, sizeof(chan));
+    gnrc_netapi_set(ccn_netif->pid, NETOPT_CHANNEL, 0, &chan, sizeof(chan));
 
     uint16_t src_len = 8U;
-    gnrc_netapi_set(hopp_netif->pid, NETOPT_SRC_LEN, 0, &src_len, sizeof(src_len));
+    gnrc_netapi_set(ccn_netif->pid, NETOPT_SRC_LEN, 0, &src_len, sizeof(src_len));
 #ifdef BOARD_NATIVE
-    gnrc_netapi_get(hopp_netif->pid, NETOPT_ADDRESS, 0, hwaddr, sizeof(hwaddr));
+    gnrc_netapi_get(ccn_netif->pid, NETOPT_ADDRESS, 0, hwaddr, sizeof(hwaddr));
 #else
-    gnrc_netapi_get(hopp_netif->pid, NETOPT_ADDRESS_LONG, 0, hwaddr, sizeof(hwaddr));
+    gnrc_netapi_get(ccn_netif->pid, NETOPT_ADDRESS_LONG, 0, hwaddr, sizeof(hwaddr));
 #endif
     gnrc_netif_addr_to_str(hwaddr, sizeof(hwaddr), hwaddr_str);
 
+/*
     hopp_pid = thread_create(hopp_stack, sizeof(hopp_stack), HOPP_THREAD_PRIORITY,
                              THREAD_CREATE_STACKTEST, hopp, &ccnl_relay, "hopp");
 
@@ -309,22 +399,26 @@ int main(void)
     }
 
     hopp_set_cb_published(cb_published);
-
+*/
     ccnl_qos_set_tcs((qos_traffic_class_t *) &tcs, sizeof(tcs) / sizeof(tcs[0]));
 
     ccnl_set_pit_strategy_remove(pit_strategy);
 
     printf("config;%d\n", ccnl_relay.max_pit_entries);
 
+    (void) rootprefix;
+    (void) rootaddr;
+
+/*
     if (memcmp(hwaddr_str, rootaddr, strlen(rootaddr)) == 0) {
         _root(0, NULL);
-        xtimer_sleep(140);
+        xtimer_sleep(210);
     }
     else {
         ccnl_set_local_producer(producer_func);
-        xtimer_sleep(10);
+        xtimer_sleep(20);
         _publish(0, NULL);
-        xtimer_sleep(80);
+        xtimer_sleep(100);
     }
 
     printf("route;%s;%u\n", hwaddr_str, dodag.rank);
@@ -333,16 +427,35 @@ int main(void)
     msg_send(&msg, hopp_pid);
     hopp_set_cb_published(NULL);
 
+    xtimer_sleep(30);
+*/
+
+    xtimer_sleep(5);
+
+    setup_forwarding(hwaddr_str);
+
+    //show_fibs(&ccnl_relay);
+
+//    msg.type = CCNL_MSG_CS_FLUSH;
+//    msg_send(&msg, ccnl_event_loop_pid);
+
+//    xtimer_sleep(5);
+
+    ccnl_set_local_producer(producer_func);
+
     if (i_am_root) {
+        xtimer_sleep(5);
+        ccnl_set_local_producer(NULL);
         puts("starting consumer");
-        memset(hopp_stack, 0, HOPP_STACKSZ);
-        thread_create(hopp_stack, sizeof(hopp_stack),
+        memset(consumer_stack, 0, CONSUMER_STACKSZ);
+        thread_create(consumer_stack, sizeof(consumer_stack),
                       CONSUMER_THREAD_PRIORITY, THREAD_CREATE_STACKTEST,
                       consumer_event_loop, NULL, "consumer");
     }
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
