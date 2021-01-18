e2bd148f34bd15ef6a38e0e5164c05277a8a3a2d
Cenk Gündoğan <mail+dev@gundogan.net>
2021-01-18 19:28:39 +0100

    apply coap default port for proxies

--- sys/net/application_layer/gcoap/forward_proxy/forward_proxy.c
+++ sys/net/application_layer/gcoap/forward_proxy/forward_proxy.c
@@ -255,14 +255,18 @@ static bool _parse_endpoint(sock_udp_ep_t *remote,
     }
     memcpy(&remote->addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
 
-    /* copy port string into scratch for atoi */
-    memcpy(scratch, urip->port, urip->port_len);
-    scratch[urip->port_len] = '\0';
+    if (urip->port_len) {
+        /* copy port string into scratch for atoi */
+        memcpy(scratch, urip->port, urip->port_len);
+        scratch[urip->port_len] = '\0';
 
-    remote->port = atoi(scratch);
+        remote->port = atoi(scratch);
 
-    if (remote->port == 0) {
-        return false;
+        if (remote->port == 0) {
+            return false;
+        }
+    } else {
+        remote->port = 5683;
     }
 
     return true;
